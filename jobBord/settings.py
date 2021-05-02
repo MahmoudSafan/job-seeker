0db4c665175718d754c48a0b5f8055d66858964f
Mahmoud Safan <46950214+MahmoudSafan@users.noreply.github.com>
2021-05-02 03:29:33 +0200

    Update settings.py

--- jobBord/settings.py
+++ jobBord/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7%$4rfr0-#bv*d6)2p#dbzq%3hl7*n_nkgl+(%rz)8g9^u3f17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
